5c56d568b3934451a677869c89116c989e5e7667
Zhenkaaf <zhenkaaf@gmail.com>
2025-05-27 11:07:53 +0300

    comments-populate

--- src/models/Comment.ts
+++ src/models/Comment.ts
@@ -2,17 +2,19 @@ import mongoose from "mongoose";
 
 const CommentSchema = new mongoose.Schema(
     {
-        comment: {
+        text: {
             type: String,
             required: true,
         },
         author: {
             type: mongoose.Schema.Types.ObjectId,
             ref: "User",
+            required: true,
         },
         postId: {
             type: mongoose.Schema.Types.ObjectId,
             ref: "Post",
+            required: true,
         },
     },
     { timestamps: true }
